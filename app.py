5227ca78fcd12af96c49e720de9ebc37a46d2304
unknown <barbosaamandacf@gmail.com>
2026-06-10 17:47:54 -0300

    adiciona gunicorn para deploy

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from database import listar_medicamentos
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    medicamentos = listar_medicamentos()
+
+    return render_template(
+        "index.html",
+        medicamentos=medicamentos
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
